8d0df5c5717fa5e1160625996fe456b31c73d60a
tomhoon <gnsdl9108@naver.com>
2024-02-15 00:54:51 +0900

    feat: 반복학습을 위한 연습 5번 끝!

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:practice/gradient_container.dart';
-
-void onPressed() {
-  print('Start quiz button is pressed');
-}
+import 'package:practice/start_screen.dart';
 
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(
-          color: Colors.deepPurple[600],
-          child: Column(
-            children: <Widget>[
-              Container(
-                child: Image.asset("images/quiz-logo.png"),
-                height: 700,
-              ),
-              Container(
-                child: Text(
-                  'Learn Flutter the fun way!',
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-              ElevatedButton(
-                onPressed: onPressed,
-                child: const Text('Start Quiz'),
-                style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all(Colors.deepPurple[600]),
-                  textStyle: MaterialStateProperty.all(
-                    TextStyle(color: Colors.white),
-                  ),
-                ),
-              )
-            ],
-          ),
-        ),
+        body: StartScreen(),
       ),
     ),
   );
