5bbdbc485b8ea7ff2c1ea8fe213c634b75d8f071
tomhoon <gnsdl9108@naver.com>
2024-02-15 00:23:52 +0900

    feat: 내가 만든 실습

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:practice/gradient_container.dart';
 
+void onPressed() {
+  print('Start quiz button is pressed');
+}
+
 void main() {
-  runApp(const MaterialApp(
+  runApp(
+    MaterialApp(
       home: Scaffold(
-    body: GradientContainer(Colors.red, Colors.blue),
-  )));
+        body: Container(
+          color: Colors.deepPurple[600],
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: Image.asset("images/quiz-logo.png"),
+                height: 700,
+              ),
+              Container(
+                child: Text(
+                  'Learn Flutter the fun way!',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: onPressed,
+                child: const Text('Start Quiz'),
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all(Colors.deepPurple[600]),
+                  textStyle: MaterialStateProperty.all(
+                    TextStyle(color: Colors.white),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
 }
